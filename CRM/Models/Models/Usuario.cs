fd4981a4c89cf19c90db8617e162289baf4e1261
Josue <Josue@DESKTOP-9HCU5TC>
2019-04-06 17:44:57 -0600

    Agregar archivos de proyecto.

---
+++ CRM/Models/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace CRM.Models.Models
+{
+    public partial class Usuario
+    {
+
+    }
+}
